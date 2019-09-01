6fa66cd74e1e6610560c48871796f4bfcbbf1066
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-09-01 14:11:55 +0200

    Completed exercise

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,27 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", start);
+
+const typeWriter = document.querySelector("#typewriter");
+const typeWriterText = typeWriter.textContent;
+
+let subStart = 0;
+let subEnd = 1;
+let letter;
+
+/* ------------------ Functions ---------------------- */
+
+function start() {
+  typeWriter.textContent = "";
+  typeWriterEffect();
+}
+
+function typeWriterEffect() {
+  if (subStart < typeWriterText.length) {
+    letter = typeWriterText.substring(subStart, subEnd);
+    typeWriter.textContent += letter;
+    subStart++;
+    subEnd++;
+    setTimeout(typeWriterEffect, 200);
+  }
+}
